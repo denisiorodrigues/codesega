23445e6481cbfb3c3a10a2beefc22ac349357d0d
Denisio Rodrigues [Mac] <denisio@ymail.com>
2018-10-02 20:23:52 -0300

    Resolvendo o problema de repetição

--- estruturas_repeticao/task_list.rb
+++ estruturas_repeticao/task_list.rb
@@ -1,18 +1,27 @@
-puts "Bem-vindo ao Task List! Escolha uma opção no menu: \n"
-puts '[1] Inserir uma tarefa'
-puts '[2] Ver todas as tarefas'
-puts
-print 'Opção escolhida: '
-
-opcao = gets.to_i
 tarefa = ""
+opcao = 0
 
-if opcao == 1
-  print 'Digite sua tarefa: '
-  tarefa = gets
-  puts
-  puts 'Tarefa cadastrada: ' + tarefa
-else
-  puts
-  puts tarefa
+while opcao != 3 do
+	system('clear')
+  puts "Bem-vindo ao Task List! Escolha uma opção no menu: \n"
+	puts '[1] Inserir uma tarefa'
+	puts '[2] Ver todas as tarefas'
+	puts '[3] Sair'
+	puts
+	print 'Opção escolhida: '
+
+	opcao = gets.to_i
+
+  if opcao == 1
+    print 'Digite sua tarefa: '
+    tarefa = gets
+    puts
+    puts 'Tarefa cadastrada: ' + tarefa
+  elsif opcao == 2
+    puts
+    puts tarefa
+  else
+    puts
+    puts 'Opção inválida'
+  end
 end
