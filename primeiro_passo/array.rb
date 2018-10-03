a634a2e06ed517e3360759cc4c70cc42f24f21d0
Denisio Rodrigues [Mac] <denisio@ymail.com>
2018-10-02 21:01:35 -0300

    Desafio do array

---
+++ primeiro_passo/array.rb
@@ -0,0 +1,31 @@
+tarefas = []
+opcao = 0
+
+while opcao != 3 do
+    system('clear')
+    puts "Bem-vindo ao Task List! Escolha uma opção no menu: \n"
+    puts '[1] Inserir uma tarefa'
+    puts '[2] Ver todas as tarefas'
+    puts '[3] Sair'
+    puts
+    print 'Opção escolhida: '
+
+    opcao = gets.to_i
+    tarefa = ""
+
+  if opcao == 1
+    print 'Digite sua tarefa: '
+    tarefa = gets
+    tarefas << tarefa
+    puts
+    puts 'Tarefa cadastrada: ' + tarefa
+    gets
+  elsif opcao == 2
+    puts
+    puts tarefas
+    gets
+  else
+    puts
+    puts 'Opção inválida'
+  end
+end
